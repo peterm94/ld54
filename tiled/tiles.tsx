22feb3128463f33f6bc775db86bba859e4afde75
Peter <peterm1994@hotmail.com>
2023-10-01 12:52:08 +1100

    started on collectables

--- tiled/tiles.tsx
+++ tiled/tiles.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="16" tileheight="16" tilecount="8" columns="8">
- <image source="tiles.png" width="128" height="16"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="tiles.png" width="128" height="32"/>
+ <tile id="7">
+  <properties>
+   <property name="ttl" type="int" value="10"/>
+  </properties>
+ </tile>
 </tileset>
